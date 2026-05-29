cae19cb7cf172a5311855bf720a645c889a6c87d
techchandra128 <tech.chandra.128@gmail.com>
2026-05-29 15:18:13 +0530

    Bump cache to v8 - force refresh

--- sw_mobile.js
+++ sw_mobile.js
@@ -1,6 +1,6 @@
 // sw_mobile.js - RAM Mobile Service Worker
 
-const CACHE = 'ram-mobile-v7';
+const CACHE = 'ram-mobile-v8';
 const STATIC = [
     '/ram_mobile/mobile.html',
     '/ram_mobile/mobile_core.css',
