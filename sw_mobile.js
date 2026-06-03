aa7b92147caa8fc5d79b3dbd288a66639718e8fd
techchandra128 <tech.chandra.128@gmail.com>
2026-06-03 17:44:50 +0530

    changing Font Size Normal Text

--- sw_mobile.js
+++ sw_mobile.js
@@ -1,6 +1,6 @@
 ﻿// sw_mobile.js - RAM Mobile Service Worker
 
-const CACHE = 'ram-mobile-v12.14';
+const CACHE = 'ram-mobile-v12.15';
 const STATIC = [
     '/ram_mobile/mobile.html',
     '/ram_mobile/mobile_core.css',
