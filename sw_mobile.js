8dd68be99d30fabf8e7001d9f8d06817ea896dd0
techchandra128 <tech.chandra.128@gmail.com>
2026-06-03 17:29:00 +0530

    Heading Text Padding Below Updated

--- sw_mobile.js
+++ sw_mobile.js
@@ -1,6 +1,6 @@
 ﻿// sw_mobile.js - RAM Mobile Service Worker
 
-const CACHE = 'ram-mobile-v12.12';
+const CACHE = 'ram-mobile-v12.13';
 const STATIC = [
     '/ram_mobile/mobile.html',
     '/ram_mobile/mobile_core.css',
