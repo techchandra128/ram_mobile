23eb63858725b95148a45b39c2f3f7bee058f012
techchandra128 <tech.chandra.128@gmail.com>
2026-06-03 17:47:43 +0530

    text left bar

--- sw_mobile.js
+++ sw_mobile.js
@@ -1,6 +1,6 @@
 ﻿// sw_mobile.js - RAM Mobile Service Worker
 
-const CACHE = 'ram-mobile-v12.15';
+const CACHE = 'ram-mobile-v12.16';
 const STATIC = [
     '/ram_mobile/mobile.html',
     '/ram_mobile/mobile_core.css',
