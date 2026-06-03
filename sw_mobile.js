246b20b9b9b388f66dc5d315bde42ebac5629869
techchandra128 <tech.chandra.128@gmail.com>
2026-06-03 18:19:45 +0530

    Vertical Bar back to 9

--- sw_mobile.js
+++ sw_mobile.js
@@ -1,6 +1,6 @@
 ﻿// sw_mobile.js - RAM Mobile Service Worker
 
-const CACHE = 'ram-mobile-v12.20';
+const CACHE = 'ram-mobile-v12.21';
 const STATIC = [
     '/ram_mobile/mobile.html',
     '/ram_mobile/mobile_core.css',
