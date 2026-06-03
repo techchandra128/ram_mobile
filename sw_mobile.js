a8a96303b0a14cfe167d6ac57f1cf7df34034027
techchandra128 <tech.chandra.128@gmail.com>
2026-06-03 17:19:44 +0530

    Heading Text Bars Updated

--- sw_mobile.js
+++ sw_mobile.js
@@ -1,6 +1,6 @@
 ﻿// sw_mobile.js - RAM Mobile Service Worker
 
-const CACHE = 'ram-mobile-v12.11';
+const CACHE = 'ram-mobile-v12.12';
 const STATIC = [
     '/ram_mobile/mobile.html',
     '/ram_mobile/mobile_core.css',
