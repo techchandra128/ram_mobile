4dc4ba82b4546d18581765fec989b5d3449b3477
techchandra128 <tech.chandra.128@gmail.com>
2026-06-04 13:34:32 +0530

    coming back to 6px

--- sw_mobile.js
+++ sw_mobile.js
@@ -1,6 +1,6 @@
 ﻿// sw_mobile.js - RAM Mobile Service Worker
 
-const CACHE = 'ram-mobile-v14.1';
+const CACHE = 'ram-mobile-v14.2';
 const STATIC = [
     '/ram_mobile/mobile.html',
     '/ram_mobile/mobile_core.css',
