3f1ff64151a89b26099354f312763a6b2abd2b36
techchandra128 <tech.chandra.128@gmail.com>
2026-06-05 09:48:47 +0530

    fix: match cornell cell text color to web version

--- sw_mobile.js
+++ sw_mobile.js
@@ -1,6 +1,6 @@
 ﻿// sw_mobile.js - RAM Mobile Service Worker
 
-const CACHE = 'ram-mobile-v14.7';
+const CACHE = 'ram-mobile-v14.8';
 const STATIC = [
     '/ram_mobile/mobile.html',
     '/ram_mobile/mobile_core.css',
