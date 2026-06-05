023f0821cd4debf1f7f8b9bee3dd5e9659a000bd
techchandra128 <tech.chandra.128@gmail.com>
2026-06-05 09:40:11 +0530

    fix: align cornell left bg color to match web version

--- sw_mobile.js
+++ sw_mobile.js
@@ -1,6 +1,6 @@
 ﻿// sw_mobile.js - RAM Mobile Service Worker
 
-const CACHE = 'ram-mobile-v14.5';
+const CACHE = 'ram-mobile-v14.6';
 const STATIC = [
     '/ram_mobile/mobile.html',
     '/ram_mobile/mobile_core.css',
